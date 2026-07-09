9e3090df823a6438571f71221cf1e49f20508870
Waqar-5 <wa5134810@gmail.com>
2026-07-09 20:09:58 +0500

    added env

--- Level2_Task1_WeatherApp/src/services/weatherService.js
+++ Level2_Task1_WeatherApp/src/services/weatherService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-console.log("API KEY:", API_KEY);
+// const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+// console.log("API KEY:", API_KEY);
 
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const GEO_URL = "https://api.openweathermap.org/geo/1.0";
